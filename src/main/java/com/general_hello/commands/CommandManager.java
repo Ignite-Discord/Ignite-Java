874160da739291ffd6064b476a8cdd50ac5493d4
General Hello <keenebrigado@gmail.com>
2021-10-08 12:45:49 +0800

    Removed music commands

--- src/main/java/com/general_hello/commands/CommandManager.java
+++ src/main/java/com/general_hello/commands/CommandManager.java
@@ -15,7 +15,6 @@ import com.general_hello.commands.commands.Info.AboutCommand;
 import com.general_hello.commands.commands.Info.InfoServerCommand;
 import com.general_hello.commands.commands.Info.InfoUserCommand;
 import com.general_hello.commands.commands.Math.MathCommand;
-import com.general_hello.commands.commands.MusicPlainCommand.*;
 import com.general_hello.commands.commands.Others.*;
 import com.general_hello.commands.commands.RankingSystem.ViewRank;
 import com.general_hello.commands.commands.Register.RegisterCommand;
@@ -88,7 +87,7 @@ public class CommandManager {
         addCommand(new ViewRank());
 
         //music
-        addCommand(new PauseCommand());
+        /*addCommand(new PauseCommand());
         addCommand(new PlayCommand());
         addCommand(new QueueCommand());
         addCommand(new RepeatCommand());
@@ -96,7 +95,7 @@ public class CommandManager {
         addCommand(new SkipCommand());
         addCommand(new VolumeCommand());
         addCommand(new StopCommand());
-        addCommand(new LeaveCommand());
+        addCommand(new LeaveCommand());*/
 
         //others
         addCommand(new JokeCommand());
